3dff9c6745e15defec3666478224eb37901c4a34
kobbok <kate.oboyle@icloud.com>
2022-07-23 14:39:03 +0100

    home page but change font at end

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 React Router Hash Link
 
+
+To Do: 
+- Page content
+- Keep 'scroll' on all pages
+- Keep navbar on all pages
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,17 @@ const Home = () => {
       <div className='scroll'>scroll &gt;</div>
       
       <div className='wrapper'>
-        <div className='slide home'>home</div>
+        <div className='slide home'>
+          <div className='home-textbox'>
+            <span id='home-header'>
+            Kate <br/>
+            O'Boyle
+            </span>
+            <br/>
+            <br/>
+            <span id='home-subheader'>Software Developer</span>
+          </div>        
+        </div>
       </div>
     </main>
   )
