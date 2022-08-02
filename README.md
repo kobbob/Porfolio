22e5f27e65b0ddd87ec58158cc61a1f0ccffe528
kobbok <kate.oboyle@icloud.com>
2022-08-02 14:03:10 +0100

    mixing up layout again

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ To Do:
 - Page content
 - Keep 'scroll' on all pages
 - Keep navbar on all pages
+- Once everything is working, refactor into components
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -4,30 +4,67 @@ const Body = () => {
   return (
     <main className='site-wrapper'>
       <div className='scroll'>scroll &gt;</div>
+    
+    
+{/* ----- Home page -----
+      <div className='home-wrapper'>
+        <div className='slide home' id='home'>
+            <span id='home-header'>
+              Kate <br /> O'Boyle
+            </span>
+            <br /><br />
+            <span id='home-subheader'>Software Developer</span>
+        </div>    
+      </div> */}
 
+
+{/* ----- Subsequent pages ----- */}
       <div className='wrapper'>
-        {/* <div className='scroll'>scroll</div> */}
-        {/* <div className='slide body' id='home'>home</div> */}
-        <div className='slide body' id='home'>
-          <span id='home-header'>
-            Kate <br />
-            O'Boyle
-          </span>
-          <br />
-          <br />
-          <span id='home-subheader'>Software Developer</span>
+
+        {/* ----- Home page ----- */}
+        <div className='slide home' id='home'>
+              <span id='home-header'>
+                Kate <br /> O'Boyle
+              </span>
+              <br /><br />
+              <span id='home-subheader'>Software Developer</span>
           </div>    
-
-
-
-
-
-          <div className='slide about' id='about'>about</div>
-          <div className='slide experience'>experience</div>
-          <div className='slide skills'>skills</div>
-          <div className='slide portfolio' id='portfolio'>portfolio</div>
-          <div className='slide contact' id='contact'>contact</div>
         </div>
+
+        {/* ----- About page ----- */}
+          <div className='slide about' id='about'>about</div>
+
+        {/* ----- Experience page ----- */}
+          <div className='slide experience' id='experience'>experience</div>
+
+        {/* ----- Skills page ----- */}
+          <div className='slide skills' id=''>skills</div>
+
+        {/* ----- Portfolio page ----- */}
+          <div className='slide portfolio' id='portfolio'>
+            <span>Portfolio</span>
+            <div className='project container1'>
+              1
+            </div>
+            <div className='project container2'>
+              2
+            </div>
+            <div className='project container3'>
+              3
+            </div>
+            <div className='project container4'>
+              4
+            </div>
+
+
+
+          </div>
+
+
+        {/* ----- Contact page ----- */}
+          <div className='slide contact' id='contact'>contact</div>
+        
+        {/* </div> */}
     </main>
   )
 }
