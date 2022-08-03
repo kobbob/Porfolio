a75e8ac9620389ff145ff5ee2807c9861221af49
kobbok <kate.oboyle@icloud.com>
2022-08-03 11:12:32 +0100

    about structure. ignore styling for now

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,27 @@
 const About = () => {
 
   return (
-      <div className="slide about">about</div>
+      <div className='slide about'>
+        <div className='about-container'>
+
+            <div className='content' id='about-text'> 
+              <h1 id='about-header'>About</h1>
+              
+              <p className='about-para'>
+              From creating spaces for the built environment to exploring what the digital realm can offer, I have been drawn to the world of programming to transfer my existing design and project management skills in creating XXXXXXX.               
+              </p>
+
+              <p className='about-para'>
+              Next paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              </p>
+            </div>
+
+            <div className='content' id='profile'>
+            </div>
+
+          
+        </div>
+      </div>
   )
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
   return (
         <div className='slide home'>
           <div className='home-textbox'>
-            <span id='home-header'>Kate <br/>O'Boyle</span>
+            <span id='home-header'>Kate <br/>O&apos;Boyle</span>
               <br/><br/>
             <span id='home-subheader'>Software Developer</span>
           </div>        
